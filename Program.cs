0700106cf0dcbd6efce1f9ba7e3aae7296aba691
Jon Reed <mr.is@outlook.com>
2015-03-25 11:05:25 -0400

    Creating Class
    Class TaxFormula

--- Program.cs
+++ Program.cs
@@ -8,8 +8,38 @@ namespace ConditionalLab
 {
     class Program
     {
+        public class TaxFormula
+        {
+            public double Length { get; set; }
+            public double Width { get; set; }
+            public double Area
+            {
+                get { return Length * Width; }
+            }
+            public double Perimeter
+            {
+                get { return ((Length * 2) + (Width * 2)); }
+            }
+            public TaxFormula() { }
+            public TaxFormula(double l, double w)
+            {
+                Length = l;
+                Width = w;
+            }
+        }
         static void Main(string[] args)
         {
+            Console.Write(" Enter the Length of the land (in Yards): ");
+            Double InputL = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Console.Write(" Enter the Width of the land (in Yards): ");
+            Double InputW = Convert.ToDouble(Console.ReadLine());
+
+            TaxFormula l = new TaxFormula(InputL, InputW);
+            Decimal TotalCost = Convert.ToDecimal((l.Area * 5) + (l.Perimeter * .75));
+
         }
     }
 }
